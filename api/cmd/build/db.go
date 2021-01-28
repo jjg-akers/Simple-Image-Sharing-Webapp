4823eb120daf847497d7560dbd3511e09261ace4
Joseph-Akers <jjg.akers@gmail.com>
2021-01-28 06:07:40 -0700

    Add Build Pkg
    Added build files for SQLDb
    Added build file for minio client

---
+++ api/cmd/build/db.go
@@ -0,0 +1,78 @@
+package build
+
+import (
+	"database/sql"
+	"log"
+
+	"github.com/urfave/cli/v2"
+)
+
+type SQLDBConfig struct {
+	UserName     string
+	Password     string
+	Host         string
+	Port         string
+	MaxOpenConns int
+	MaxIdleConns int
+}
+
+func NewSQLDBConfig() *SQLDBConfig {
+	return &SQLDBConfig{}
+}
+
+func LoadSQLDBConfig(config *SQLDBConfig) []cli.Flag {
+	return []cli.Flag{
+		&cli.StringFlag{
+			Name:        "mysql-user",
+			EnvVars:     []string{"MYSQL_USER"},
+			Destination: &config.UserName,
+		},
+		&cli.StringFlag{
+			Name:        "mysql-passwor",
+			EnvVars:     []string{"MYSQL_PASSWORD"},
+			Destination: &config.Password,
+		},
+		&cli.StringFlag{
+			Name:        "mysql-host",
+			EnvVars:     []string{"MYSQL_HOST"},
+			Destination: &config.Host,
+		},
+		&cli.StringFlag{
+			Name:        "mysql-port",
+			EnvVars:     []string{"MYSQL_PORT"},
+			Destination: &config.Port,
+		},
+		&cli.IntFlag{
+			Name:        "mysql-max-open-conns",
+			EnvVars:     []string{"MYSQL_MAX_OPEN_CONNS"},
+			Destination: &config.MaxOpenConns,
+			Value:       20,
+		},
+		&cli.IntFlag{
+			Name:        "mysql-max-idle-conns",
+			EnvVars:     []string{"MYSQL_MAX_IDLE_CONNS"},
+			Destination: &config.MaxIdleConns,
+			Value:       20,
+		},
+	}
+}
+
+func NewSQLDB(config *SQLDBConfig) (*sql.DB, error) {
+
+	dsn := config.UserName + ":" + config.Password + "@tcp(" + config.Host + ":" + config.Port + ")/"
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		log.Println("error opening DB: ", err)
+		return nil, err
+	}
+
+	db.SetMaxOpenConns(config.MaxOpenConns)
+	db.SetMaxIdleConns(config.MaxIdleConns)
+
+	if err := db.Ping(); err != nil {
+		log.Println("error ping DB: ", err)
+		return nil, err
+	}
+
+	return db, nil
+}
---
+++ api/cmd/build/remotestorage.go
@@ -0,0 +1,46 @@
+package build
+
+import "github.com/urfave/cli/v2"
+
+type RemoteStorageConfig struct {
+	Endpoint        string
+	AccessKeyID     string
+	AccessKeySecret string
+	useSSL          bool
+}
+
+func NewRemoteSotrageConfig() *RemoteStorageConfig {
+	return &RemoteStorageConfig{}
+}
+
+func LoadStorageConfig(config *RemoteStorageConfig) []cli.Flag {
+	//var flags []cli.Flag
+	return []cli.Flag{
+		&cli.StringFlag{
+			Name:        "remote-storage-endpoint",
+			EnvVars:     []string{"REMOTE_STORAGE_ENDPOINT"},
+			Destination: &config.Endpoint,
+		},
+		&cli.StringFlag{
+			Name:        "remote-storage-access-key-id",
+			EnvVars:     []string{"REMOTE_STORAGE_ACCESS_KEY_ID"},
+			Destination: &config.AccessKeyID,
+		},
+		&cli.StringFlag{
+			Name:        "remote-storage-access-key-secret",
+			EnvVars:     []string{"REMOTE_STORAGE_ACCESS_KEY_SECRET"},
+			Destination: &config.AccessKeySecret,
+		},
+		&cli.BoolFlag{
+			Name:        "remote-storage-use-ssl",
+			EnvVars:     []string{"REMOTE_STORAGE_USE_SSL"},
+			Destination: &config.useSSL,
+			Value:       false,
+		},
+	}
+}
+
+// endpoint := "localhost:9000"
+// accessKeyID := "minioadmin"
+// secretAccessKey := "minioadmin"
+// useSSL := false
